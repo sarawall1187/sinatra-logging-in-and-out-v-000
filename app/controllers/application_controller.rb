1f98aeb29acbebd091472d9ab29da66cdd528bee
Sara Walker <sarawall1187@gmail.com>
2019-03-16 15:32:14 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     @user = User.find_by(:username => params[:username])
 
-    @user = User.find_by(params[:username])
+    # @user = User.find_by(params[:username])
     if @user
       session[:user_id] = @user.id
       # session[:username] = @user.username
