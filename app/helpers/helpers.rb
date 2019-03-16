49963a2b4cacdb47ffd8e74e658e09e8bf9e500f
Sara Walker <sarawall1187@gmail.com>
2019-03-16 15:31:07 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -5,12 +5,8 @@ class Helpers
   end
 
   def self.is_logged_in?(session)
-<<<<<<< HEAD
      !!session[:user_id]
-=======
     @user = User.find(params[:id])
     #  if session[:user_id] == @user.id
-
->>>>>>> 6aba98d60ae5eb5c2cac47074c78e2a39eacad75
-  end
+    end
 end
