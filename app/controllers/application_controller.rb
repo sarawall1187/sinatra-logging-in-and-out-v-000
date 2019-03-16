92eacea805980af15af423ccaf966742afbd9f47
Sara Walker <sarawall1187@gmail.com>
2019-03-16 15:30:35 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,12 +12,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-<<<<<<< HEAD
     @user = User.find_by(:username => params[:username])
-    # binding.pry
-=======
+
     @user = User.find_by(params[:username])
->>>>>>> 6aba98d60ae5eb5c2cac47074c78e2a39eacad75
     if @user
       session[:user_id] = @user.id
       # session[:username] = @user.username
@@ -25,13 +22,11 @@ class ApplicationController < Sinatra::Base
     else
       redirect to '/error'
     end
-<<<<<<< HEAD
+
   end
 
   get '/error' do
     erb :error
-=======
->>>>>>> 6aba98d60ae5eb5c2cac47074c78e2a39eacad75
   end
 
   get '/account' do
